1d3fb84db7a03004015852c1adfda3e565832c42
developer-vishwa <vishwajitshindevp753@gmail.com>
2023-12-20 12:00:31 +0530

    structure create

--- examportal/src/main/java/com/examportal/ExamportalApplication.java
+++ examportal/src/main/java/com/examportal/ExamportalApplication.java
@@ -5,10 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExamportalApplication {
-
-
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(ExamportalApplication.class, args);
 	}
---
+++ examportal/src/main/java/com/examportal/domain/user.java
@@ -0,0 +1,4 @@
+package com.examportal.domain;
+
+public class user {
+}
