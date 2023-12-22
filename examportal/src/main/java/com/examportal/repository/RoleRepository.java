f1b5cbaf6d1464bd5472ceaf0f9971c4a30a39d0
developer-vishwa <vishwajitshindevp753@gmail.com>
2023-12-22 13:35:25 +0530

    User Registration Simple Web Serivces

--- examportal/src/main/java/com/examportal/repository/RoleRepository.java
+++ examportal/src/main/java/com/examportal/repository/RoleRepository.java
@@ -1,2 +1,9 @@
-package com.examportal.repository;public interface RoleRepository {
+package com.examportal.repository;
+
+import com.examportal.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role,Long> {
 }
--- examportal/src/main/java/com/examportal/service/impl/UserServiceImpl.java
+++ examportal/src/main/java/com/examportal/service/impl/UserServiceImpl.java
@@ -1,2 +1,39 @@
-package com.examportal.service.impl;public class UserServiceImpl {
+package com.examportal.service.impl;
+
+import com.examportal.domain.Role;
+import com.examportal.domain.User;
+import com.examportal.domain.UserRole;
+import com.examportal.repository.RoleRepository;
+import com.examportal.repository.UserRepository;
+import com.examportal.service.UserServices;
+import org.hibernate.bytecode.internal.bytebuddy.BytecodeProviderImpl;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@Service
+public class UserServiceImpl implements UserServices {
+
+
+//    private static final Logger logger = (Logger) LoggerFactory.getLogger(UserServices.class);
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User createUser(User user) {
+        User currentUser = this.userRepository.findByUserName(user.getUserName());
+        if (currentUser != null) {
+//            logger.info("this User Already Exist");
+        } else {
+            currentUser = this.userRepository.save(user);
+        }
+        return currentUser;
+    }
 }
